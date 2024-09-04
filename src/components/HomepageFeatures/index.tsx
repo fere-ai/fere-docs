856fbf5b815d570329814d1a85c6537a97441efb
r48b1t <arc09@pm.me>
2024-09-05 01:42:15 +0530

    Some updates to homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -6,11 +6,12 @@ type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  link: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Docs',
+    title: 'Product Docs',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -19,9 +20,10 @@ const FeatureList: FeatureItem[] = [
         via Investment Agent. These docs will help you get started.
       </>
     ),
+    link: '/docs/intro',
   },
   {
-    title: 'Build',
+    title: 'Developer',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -30,6 +32,7 @@ const FeatureList: FeatureItem[] = [
         own trading bot or platform. The possibilities are endless.
       </>
     ),
+    link: '/docs/api/api-introduction',
   },
   {
     title: 'Support',
@@ -39,19 +42,28 @@ const FeatureList: FeatureItem[] = [
         Get Support on product usage or API integration. We are here to help you.
       </>
     ),
+    link: 'https://discord.com/invite/3fsm5XJNW8',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, description, link}: FeatureItem) {
+  const isExternalLink = link.startsWith('http') || link.startsWith('https');
+
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={clsx(styles.herocard, 'col col--4')}>
+      <a
+        href={link}
+        target={isExternalLink ? "_blank" : undefined}
+        rel={isExternalLink ? "noopener noreferrer" : undefined}
+      >
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+        <div className="text--center padding-horiz--md">
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
+      </a>
     </div>
   );
 }
@@ -60,7 +72,7 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className={clsx('row', styles.featureRow)}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
